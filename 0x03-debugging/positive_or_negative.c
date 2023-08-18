f54b1b5702c42bde6c501675062fdb89ed3db099
Mariam <mariam.aamk@gmail.com>
2023-08-18 15:30:09 -0700

    0-main

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * main - entry point
+ *
+ * Description: tests function that prints if integer is positive or negative
+ * Return: void
+
 void positive_or_negative(int i)
 {
 	if (i > 0)
