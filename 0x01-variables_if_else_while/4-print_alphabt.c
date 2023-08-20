b87178dffac2ab209bd5d276add382302f166f1e
Mariam <mariam.aamk@gmail.com>
2023-08-19 18:45:17 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'print alphabets in lowercase except q and e'
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	int a = 97;
+
+	while (a <= 122)
+	{
+		if (a == 101 || a == 113)
+		{
+			a++;
+			continue;
+		}
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+
+		return (0);
+}
