bebf7eae121f84dd849b89637101267c8199c2f6
Mariam <mariam.aamk@gmail.com>
2023-09-14 23:36:02 -0700

    c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	if (separator == NULL)
 		separator = "";
-	
+
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, char *);
